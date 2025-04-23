11b6979b320bd8ea48141cb98df3973b240d156b
sara_ahmadd <saraahmad2060039@gmail.com>
2025-04-23 23:09:40 +0200

    update some imports  for deployment

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -3,5 +3,6 @@ import { createParamDecorator } from '@nestjs/common';
 export const User = createParamDecorator((data, context) => {
   const req = context.switchToHttp().getRequest();
   const user = req.user;
+  //get all user object or just some data
   return data ? user[data] : user;
 });
