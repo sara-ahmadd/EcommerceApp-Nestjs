0f2ac547519a8c50fed15663a08b39c57aa5686f
sara_ahmadd <saraahmad2060039@gmail.com>
2025-04-25 20:49:40 +0300

    fixing deployment issues

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { CategoryRepository } from './category.repository';
+import { CategoryModel } from './../../DB/models/category.model';
 
 @Module({
+  imports: [CategoryModel],
   controllers: [CategoryController],
   providers: [CategoryService, CategoryRepository],
   exports: [CategoryRepository],
